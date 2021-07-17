b2772a47982ab7a614533f73f9423973aee2e0ed
Marlon Santos <xsantos.marlonx@gmail.com>
2021-07-17 09:49:16 -0300

    Foi implementado o metodo ToString

--- Course/Product.cs
+++ Course/Product.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 
 namespace Course
 {
@@ -15,6 +11,17 @@ namespace Course
         public double ValorTotalEmEstoque()
         {
             return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + " $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + "unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 }
